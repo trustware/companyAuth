3d47a668dba58f425ba0dbb61b76f754eb02cbf5
Patrick Korth <pkorth@umich.edu>
2015-03-25 12:48:25 -0400

    Syntax error in main

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ urls = (
 )
 
 class index:
-def GET(self):
-  return "Hello, world!"
+  def GET(self):
+    return "Hello, world!"
 
 if __name__ == "__main__":
   app = web.application(urls, globals())
