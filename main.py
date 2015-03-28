5d8a7d1b85adb4acac0aa9bf3dc44d27e9ebbdff
Patrick Korth <pkorth@umich.edu>
2015-03-28 13:51:26 -0400

    Connect to SQL database

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
 from flask import Flask
+import os
+import psycopg2
+import urlparse
+import sys
+
 
 app = Flask(__name__)
 
@@ -9,6 +13,26 @@ def hello():
     return "Hello world!"
 
 
-if __name__ == "__main__":
+def main(argc, argv):
+  # Connect to SQL database
+  urlparse.uses_netloc.append('postgres')
+  if not os.environ.has_key('DATABASE_URL'):
+    print 'DATABASE_URL environment variable not found'
+    print 'Exiting...'
+    return
+  url = urlparse.urlparse(os.environ['DATABASE_URL'])
+  try:
+    conn = psycopg2.connect(dbname=url.path[1:], user=url.username,
+        password=url.password, host=url.hostname)
+  except psycopg2.Error as e:
+    print 'Could not connect to database:', e.pgerror
+    print 'Exiting...'
+    return
+
+  # Start accepting requests
   port = int(os.environ.get("PORT", 8080))
   app.run(host='0.0.0.0', port=port)
+
+
+if __name__ == "__main__":
+  main(len(sys.argv), sys.argv)
